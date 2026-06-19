f4e144b46c77f17fbf8a45a83ec65f8f5fd319d4
selimsahindev <selimsahin.sns@gmail.com>
2026-06-20 02:23:26 +0300

    chore(build): initialize gradle multi-module project

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+allprojects {
+    group = "dev.selimsahin.kunefe"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "kunefe"
+
+include(
+    "kunefe-proto",
+    "kunefe-broker",
+    "kunefe-client",
+    "kunefe-spring-client",
+    "kunefe-spring-boot-starter",
+    "kunefe-test"
+)
