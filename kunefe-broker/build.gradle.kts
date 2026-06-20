aec8d094a0979436f95d66d2541fd0f20a0c7841
selimsahindev <selimsahin.sns@gmail.com>
2026-06-20 23:02:42 +0300

    chore(broker): add spring boot entry point

---
+++ kunefe-broker/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("com.google.protobuf") version "0.9.4"
+}
+
+dependencies {
+    implementation(project(":kunefe-proto"))
+
+    // gRPC
+    implementation("io.grpc:grpc-netty-shaded:1.64.0")
+    implementation("io.grpc:grpc-protobuf:1.64.0")
+    implementation("io.grpc:grpc-stub:1.64.0")
+    implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
+
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    // RocksDB
+    implementation("org.rocksdb:rocksdbjni:9.2.1")
+}
---
+++ kunefe-broker/src/main/java/dev/selimsahin/kunefe/broker/KunefeBrokerApplication.java
@@ -0,0 +1,12 @@
+package dev.selimsahin.kunefe.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunefeBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunefeBrokerApplication.class, args);
+    }
+}
